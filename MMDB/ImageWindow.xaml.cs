7b6e21d1e04ac2cbae1bb096d21a65504173801b
maciejsobotka <sobot92@tlen.pl>
2016-10-04 21:54:16 +0200

    Added function that changes two determined pixels and another that counts avg color of picture.

--- MMDB/ImageWindow.xaml.cs
+++ MMDB/ImageWindow.xaml.cs
@@ -21,12 +21,52 @@ namespace MMDB
     {
         public ImageWindow()
         {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
             InitializeComponent();
         }
 
-        public void setImage(string imgPath)
+        public void SetImage(string imgPath)
         {
-            canvas.Source = new BitmapImage(new Uri(imgPath));
+            BitmapImage img = new BitmapImage(new Uri(imgPath));
+            SetCanvasSize(img.PixelWidth, img.PixelHeight);
+            canvas.Source = img;
+        }
+
+        public void SetImage(int width, int height, double dpiX, double dpiY, PixelFormat pf, byte[] pixels, int stride)
+        {
+            BitmapSource img = BitmapSource.Create(
+                width,
+                height,
+                dpiX,
+                dpiY,
+                pf,
+                /* palette: */ null,
+                pixels,
+                stride);
+            SetCanvasSize(img.PixelWidth, img.PixelHeight);
+            canvas.Source = img;
+        }
+
+        private void SetCanvasSize(int pixelWidth, int pixelHeight)
+        {
+            canvas.Height = 600;
+            canvas.Width = 600;
+            if (pixelHeight > pixelWidth)
+                if (canvas.Height > pixelHeight)
+                {
+                    canvas.Height = pixelHeight;
+                    canvas.Width = pixelWidth;
+                }
+                else
+                    canvas.Width = (Height / pixelHeight) * pixelWidth;
+            if (pixelHeight < pixelWidth)
+                if (canvas.Width > pixelWidth)
+                {
+                    canvas.Height = pixelHeight;
+                    canvas.Width = pixelWidth;
+                }
+                else
+                    canvas.Height = (Width / pixelWidth) * pixelHeight;
         }
     }
 }
--- MMDB/MainWindow.xaml.cs
+++ MMDB/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace MMDB
     public partial class MainWindow : Window
     {
         private string imgPath = "";
+        private ImageWindow imgWindow;
         public MainWindow()
         {
             InitializeComponent();
@@ -44,13 +45,61 @@ namespace MMDB
         {
             if (File.Exists(imgPath))
             {
-                ImageWindow imgWindow = new ImageWindow();
-                imgWindow.setImage(imgPath);
+                imgWindow = new ImageWindow();
+                imgWindow.SetImage(imgPath);
                 imgWindow.Show();
-                MessageBox.Show("Done!", "Img showed");
+                changePixelButton.IsEnabled = true;
+                avgColorButton.IsEnabled = true;
             }
             else
                 MessageBox.Show("Incorrect file path!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void changePixelButton_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource img = imgWindow.canvas.Source as BitmapSource;
+            int stride = img.PixelWidth * 4;                // 4*8
+            int size = img.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            img.CopyPixels(pixels, stride, 0);
+            int index = GetPixelIndex(10,10,stride);
+            int index2 = GetPixelIndex(35, 50, stride);
+
+            pixels[index] = 0;
+            pixels[index+1] = 0;
+            pixels[index+2] = 255;
+
+            pixels[index2] = 0;
+            pixels[index2 + 1] = 255;
+            pixels[index2 + 2] = 255;
+          
+            imgWindow.SetImage(img.PixelWidth, img.PixelHeight, img.DpiX, img.DpiY, PixelFormats.Bgr32, pixels, stride);
+        }
+
+        private void avgColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource img = imgWindow.canvas.Source as BitmapSource;
+            int stride = img.PixelWidth * 4;                // 4*8
+            int size = img.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            img.CopyPixels(pixels, stride, 0);
+            int red=0, green=0, blue=0;
+            for(int i=0; i < img.PixelWidth; ++i)
+                for(int j=0; j< img.PixelHeight; ++j)
+                {
+                    var index = GetPixelIndex(i, j, stride);
+                    red += pixels[index];
+                    green += pixels[index + 1];
+                    blue += pixels[index + 2];
+                }
+            red = red / (img.PixelWidth * img.PixelHeight);
+            green = green / (img.PixelWidth * img.PixelHeight);
+            blue = blue / (img.PixelWidth * img.PixelHeight);
+            colorRectangle.Fill = new SolidColorBrush(Color.FromRgb((byte) red, (byte) green,(byte)  blue));
+        }
+        private int GetPixelIndex(int x, int y, int stride)
+        {
+            return y * stride + 4 * x;
         }
     }
 }
