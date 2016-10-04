8e72f76eefc35108efcc1e5562336dab52d38a03
maciejsobotka <sobot92@tlen.pl>
2016-10-04 18:58:37 +0200

    Initial commit. Loading of Image

---
+++ MMDB/ImageWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MMDB
+{
+    /// <summary>
+    /// Interaction logic for ImageWindow.xaml
+    /// </summary>
+    public partial class ImageWindow : Window
+    {
+        public ImageWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void setImage(string imgPath)
+        {
+            canvas.Source = new BitmapImage(new Uri(imgPath));
+        }
+    }
+}
---
+++ MMDB/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MMDB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string imgPath = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void buttonImgSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "JPEG |*.JPG;*.JPEG";
+            dlg.Multiselect = false;  // default
+            dlg.ShowDialog();
+            imgPath = dlg.FileName;
+            textBoxSource.Text = "";
+            textBoxSource.Foreground = Brushes.Black;
+            textBoxSource.FontStyle = FontStyles.Normal;
+            textBoxSource.Text = imgPath;
+        }
+
+        private void buttonShowImg_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(imgPath))
+            {
+                ImageWindow imgWindow = new ImageWindow();
+                imgWindow.setImage(imgPath);
+                imgWindow.Show();
+                MessageBox.Show("Done!", "Img showed");
+            }
+            else
+                MessageBox.Show("Incorrect file path!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
